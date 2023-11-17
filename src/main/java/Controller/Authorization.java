e12589ce94a87128e82c9ca28a13314c6aaddfbf
jaennil <jaennil@yandex.ru>
2023-11-17 23:18:38 +0300

    auth

--- src/main/java/Controller/Authorization.java
+++ src/main/java/Controller/Authorization.java
@@ -6,8 +6,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,14 +16,16 @@ public class Authorization implements Initializable {
     public Label emptyIdNumberLabel;
     public Label emptyPasswordLabel;
     public Button signInButton;
+    public TextField captchaField;
+    public Label captchaInfoLabel;
     @FXML
     private TextField idNumberField;
     @FXML
     private TextField passwordField;
+    @FXML
+    private ImageView imageView;
 
     public void onClickSignInButton(ActionEvent actionEvent) {
-        model.setIdNumber(idNumberField.getText());
-        model.setPassword(passwordField.getText());
         model.signIn();
     }
 
@@ -33,11 +35,20 @@ public class Authorization implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        idNumberField.textProperty().addListener((observable, oldValue, newValue) -> model.setIdNumber(newValue));
-        passwordField.textProperty().addListener((observable, oldValue, newValue) -> model.setPassword(newValue));
+        model.generateCaptcha();
+        idNumberField.textProperty().bindBidirectional(model.idNumber);
+        passwordField.textProperty().bindBidirectional(model.password);
+        idNumberField.textProperty().addListener((observable, oldValue, newValue) -> model.highlightIdNumber());
+        passwordField.textProperty().addListener((observable, oldValue, newValue) -> model.highlightPassword());
+        captchaField.textProperty().bindBidirectional(model.captchaInputProperty);
+        captchaInfoLabel.textProperty().bindBidirectional(model.captchaInfoProperty);
+
+        signInButton.disableProperty().bindBidirectional(model.tooManyAttempts);
 
         idNumberField.styleProperty().bindBidirectional(model.idNumberFieldStyle);
         passwordField.styleProperty().bindBidirectional(model.passwordFieldStyle);
+
+        imageView.imageProperty().bindBidirectional(model.captchaImage);
 
         emptyIdNumberLabel.visibleProperty().bindBidirectional(model.idNumberEmpty);
         emptyPasswordLabel.visibleProperty().bindBidirectional(model.passwordEmpty);
--- src/main/java/Model/Authorization.java
+++ src/main/java/Model/Authorization.java
@@ -3,13 +3,18 @@ package Model;
 import Other.AuthenticatedUser;
 import Other.Database;
 import Other.Hash;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import com.mewebstudio.captcha.Captcha;
+import com.mewebstudio.captcha.GeneratedCaptcha;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.*;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.util.Duration;
 import org.example.App;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -17,33 +22,42 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Authorization {
-    public final StringProperty idNumberFieldStyle = new SimpleStringProperty();
-    public final StringProperty passwordFieldStyle = new SimpleStringProperty();
-    public final StringProperty idNumber = new SimpleStringProperty();
-    public final StringProperty password = new SimpleStringProperty();
+    public final StringProperty idNumberFieldStyle = new SimpleStringProperty("");
+    public final StringProperty passwordFieldStyle = new SimpleStringProperty("");
+    public final StringProperty idNumber = new SimpleStringProperty("");
+    public final StringProperty password = new SimpleStringProperty("");
+    public final StringProperty captchaInputProperty = new SimpleStringProperty("");
+    public final StringProperty captchaInfoProperty = new SimpleStringProperty("");
     public BooleanProperty idNumberEmpty = new SimpleBooleanProperty(false);
     public BooleanProperty passwordEmpty = new SimpleBooleanProperty(false);
+    public BooleanProperty tooManyAttempts = new SimpleBooleanProperty(false);
     private final String highlightedStyle = "-fx-border-color: red; -fx-border-width: 2px;";
     private final String defaultStyle = "";
+    public final ObjectProperty<Image> captchaImage = new SimpleObjectProperty<>();
+    private int captchaAttempts = 0;
+    private String captchaCode;
 
-    public void setIdNumber(String string) {
-        this.idNumber.set(string);
-        idNumberFieldStyle.set(string.isBlank() ? highlightedStyle : defaultStyle);
-        idNumberEmpty.set(string.isBlank());
+
+    public void highlightIdNumber() {
+        String idNumber = this.idNumber.get();
+        idNumberFieldStyle.set(idNumber.isBlank() ? highlightedStyle : defaultStyle);
+        idNumberEmpty.set(idNumber.isBlank());
     }
 
-    public void setPassword(String string) {
-        this.password.set(string);
-        passwordFieldStyle.set(string.isBlank() ? highlightedStyle : defaultStyle);
-        passwordEmpty.set(string.isBlank());
+    public void highlightPassword() {
+        String password = this.password.get();
+        passwordFieldStyle.set(password.isBlank() ? highlightedStyle : defaultStyle);
+        passwordEmpty.set(password.isBlank());
     }
 
-    public void signIn() {
-        if (idNumberEmpty.get() || passwordEmpty.get())
+    private void handleCorrectCaptcha() {
+        System.out.println("captcha true");
+        if (idNumber.get().isBlank() || password.get().isBlank())
             return;
+        System.out.println(idNumber.get());
         Database database = Database.getInstance();
         String hash = Hash.toString(Hash.hash(password.get()));
-        String statement = "SELECT * FROM people WHERE idNumber = ? AND password_hash = ?";
+        String statement = "SELECT * FROM Participants WHERE idNumber = ? AND password_hash = ?";
         Connection connection = database.getConnection();
         try (PreparedStatement preparedStatement = connection.prepareStatement(statement)) {
             preparedStatement.setString(1, idNumber.get());
@@ -60,6 +74,39 @@ public class Authorization {
         loadCorrespondingView();
     }
 
+    private void handleWrongCaptcha() {
+        captchaInfoProperty.set("Wrong captcha. try again");
+        System.out.println("captcha false");
+        captchaAttempts++;
+        System.out.println(captchaAttempts);
+        if (captchaAttempts == 3) {
+            captchaInfoProperty.set("Authorization locked for 10 seconds. Please wait");
+            System.out.println("system locked for 10 seconds");
+            captchaAttempts = 0;
+            tooManyAttempts.set(true);
+            Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(10), event -> {
+                System.out.println("unlocked");
+                tooManyAttempts.set(false);
+                captchaInfoProperty.set("");
+            }));
+            timeline.setCycleCount(1);
+            timeline.play();
+        }
+    }
+
+    public void signIn() {
+        String captchaInput = captchaInputProperty.get();
+        if (captchaInput == null) {
+            handleWrongCaptcha();
+            return;
+        }
+        if (captchaInput.equals(captchaCode)) {
+            handleCorrectCaptcha();
+            return;
+        }
+        handleWrongCaptcha();
+    }
+
     private void loadCorrespondingView() {
         AuthenticatedUser authenticatedUser = AuthenticatedUser.getInstance();
         try {
@@ -67,6 +114,17 @@ public class Authorization {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void generateCaptcha() {
+        Captcha captcha = new Captcha();
+        captcha.getConfig().setLength(4);
+        GeneratedCaptcha generated = captcha.generate();
+        BufferedImage buffered = generated.getImage();
+        Image image = SwingFXUtils.toFXImage(buffered, null);
+        captchaImage.set(image);
+        captchaCode = generated.getCode();
+        System.out.println(captchaCode);
     }
 
     public void showWrongCredentialsDialog() {
